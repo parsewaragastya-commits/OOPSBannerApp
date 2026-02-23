3f96f2dff429d7027a535db3312202d18104ba30
parsewaragastya-commits <parsewaragastya@gmail.com>
2026-02-23 20:05:31 +0530

    UC4: Banner Using string array and enhanced for loop

---
+++ uc4.java
@@ -0,0 +1,62 @@
+public class UC4 {
+
+    public static void main(String[] args) {
+
+        String[] O1 = {
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
+
+        String[] O2 = {
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
+
+        String[] P = {
+                " ******* ",
+                " *     * ",
+                " *     * ",
+                " ******* ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+
+        String[] S = {
+                "  *****  ",
+                " *     * ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                " *     * ",
+                "  *****  "
+        };
+
+        // Create Banner Array
+        String[] banner = new String[7];
+
+        // Populate Banner Using String.join()
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ",
+                    O1[i],
+                    O2[i],
+                    P[i],
+                    S[i]);
+        }
+
+        // Print Using Enhanced For Loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
