fcdb1c9399df7d4d356ad2cfadceef1b679d5856
parsewaragastya-commits <parsewaragastya@gmail.com>
2026-02-24 10:47:11 +0530

    updated uc4 implementation

--- uc4.java
+++ uc4.java
@@ -1,62 +1,62 @@
-public class UC4 {
+public class uc4 {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        String[] O1 = {
-                "  *****  ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                "  *****  "
-        };
+                String[] O1 = {
+                                "  *****  ",
+                                " *     * ",
+                                " *     * ",
+                                " *     * ",
+                                " *     * ",
+                                " *     * ",
+                                "  *****  "
+                };
 
-        String[] O2 = {
-                "  *****  ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                "  *****  "
-        };
+                String[] O2 = {
+                                "  *****  ",
+                                " *     * ",
+                                " *     * ",
+                                " *     * ",
+                                " *     * ",
+                                " *     * ",
+                                "  *****  "
+                };
 
-        String[] P = {
-                " ******* ",
-                " *     * ",
-                " *     * ",
-                " ******* ",
-                " *       ",
-                " *       ",
-                " *       "
-        };
+                String[] P = {
+                                " ******* ",
+                                " *     * ",
+                                " *     * ",
+                                " ******* ",
+                                " *       ",
+                                " *       ",
+                                " *       "
+                };
 
-        String[] S = {
-                "  *****  ",
-                " *     * ",
-                " *       ",
-                "  *****  ",
-                "       * ",
-                " *     * ",
-                "  *****  "
-        };
+                String[] S = {
+                                "  *****  ",
+                                " *     * ",
+                                " *       ",
+                                "  *****  ",
+                                "       * ",
+                                " *     * ",
+                                "  *****  "
+                };
 
-        // Create Banner Array
-        String[] banner = new String[7];
+                // Create Banner Array
+                String[] banner = new String[7];
 
-        // Populate Banner Using String.join()
-        for (int i = 0; i < 7; i++) {
-            banner[i] = String.join(" ",
-                    O1[i],
-                    O2[i],
-                    P[i],
-                    S[i]);
+                // Populate Banner Using String.join()
+                for (int i = 0; i < 7; i++) {
+                        banner[i] = String.join(" ",
+                                        O1[i],
+                                        O2[i],
+                                        P[i],
+                                        S[i]);
+                }
+
+                // Print Using Enhanced For Loop
+                for (String line : banner) {
+                        System.out.println(line);
+                }
         }
-
-        // Print Using Enhanced For Loop
-        for (String line : banner) {
-            System.out.println(line);
-        }
-    }
 }
