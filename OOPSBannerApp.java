96c0edcde471de7fe45907f747212e1f786f1e51
parsewaragastya-commits <parsewaragastya@gmail.com>
2026-03-11 09:10:11 +0530

    uc6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,66 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String[] O = getOPattern();
+        String[] P = getPPattern();
+        String[] S = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ",
+                    O[i],
+                    O[i], // second O
+                    P[i],
+                    S[i]);
+        }
+
+        // Print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 
+    // Helper method for O
+    public static String[] getOPattern() {
+
+        return new String[] {
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    // Helper method for P
+    public static String[] getPPattern() {
+
+        return new String[] {
+                " ******* ",
+                " *     * ",
+                " *     * ",
+                " ******* ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    // Helper method for S
+    public static String[] getSPattern() {
+
+        return new String[] {
+                "  *****  ",
+                " *     * ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                " *     * ",
+                "  *****  "
+        };
+    }
 }
